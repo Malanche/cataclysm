c7d8ef9965b0513e46b982822878ab0423a99b44
Carlos <carlos@Carloss-MacBook-Air.local>
2024-02-10 13:52:23 -0600

    Removed async trait crate

--- cataclysm-ws/src/web_socket_thread.rs
+++ cataclysm-ws/src/web_socket_thread.rs
@@ -1,20 +1,22 @@
+use std::future::Future;
 use crate::Message;
 
 /// Trait necessary to start a ws read-processing thread
-#[async_trait::async_trait]
-pub trait WebSocketThread: Send {
+pub trait WebSocketThread: Send + 'static {
     type Output: Send;
     /// On opened connection
     ///
     /// This function gets called when the websockets connection is properly stablished.
-    async fn on_open(&mut self) {}
+    fn on_open(&mut self) -> impl Future<Output = ()> + Send {
+        async {}
+    }
     /// On message callback
     ///
     /// This function gets called back when a [Message](crate::Message) is received.
-    async fn on_message(&mut self, message: Message);
+    fn on_message(&mut self, message: Message) -> impl Future<Output = ()> + Send;
     
     /// On closed connection
     ///
     /// This function gets called when the websockets connection is closed (either gracefully, or by an error)
-    async fn on_close(&mut self, _clean: bool) -> Self::Output;
+    fn on_close(&mut self, _clean: bool) -> impl Future<Output = Self::Output> + Send;
 }
